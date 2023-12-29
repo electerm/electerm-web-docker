4f552f964ec856b3b3b1eadcf5064edef21f02d6
ZHAO Xudong <zxdong@gmail.com>
2023-12-29 10:37:00 +0800

    Edit run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,11 @@
 
 
-docker run --init -v "/Users/home/Library/Application Support/electerm":/home/electerm/data \
+docker run --init -v "/Users/home/dev/electerm-web/data":/home/electerm/data \
   -e "DB_PATH=/home/electerm/data" \
   -e "HOST=0.0.0.0" \
   -e "SERVER=http://127.0.0.1:8082" \
+  # -e "SERVER_SECRET=45dfds!gdfD534" \
+  # -e "SERVER_PASS=1" \
+  # -e "ENABLE_AUTH=1" \
   -p 8082:5577 \
   zxdong262/electerm-web
