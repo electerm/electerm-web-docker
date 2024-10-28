4e06ed7fd1140b54f8e23352a20858c95df023ad
ZHAO Xudong <zxdong@gmail.com>
2024-10-28 20:28:18 +0800

    Code sync

--- check.sh
+++ check.sh
@@ -1,4 +1,4 @@
 docker run -it zxdong262/electerm-web sh
 docker push zxdong262/electerm-web
-docker tag zxdong262/electerm-web:latest zxdong262/electerm-web:2.40.18
-docker push zxdong262/electerm-web:2.40.18
+docker tag zxdong262/electerm-web:latest zxdong262/electerm-web:2.40.20
+docker push zxdong262/electerm-web:2.40.20
