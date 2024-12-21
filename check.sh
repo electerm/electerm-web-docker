796c311e83813c272f3ef5343887c71de7067880
ZHAO Xudong <zxdong@gmail.com>
2024-12-21 19:04:38 +0800

    Update version

--- check.sh
+++ check.sh
@@ -1,4 +1,4 @@
 docker run -it zxdong262/electerm-web sh
 docker push zxdong262/electerm-web
-docker tag zxdong262/electerm-web:latest zxdong262/electerm-web:2.51.3
-docker push zxdong262/electerm-web:2.51.3
+docker tag zxdong262/electerm-web:latest zxdong262/electerm-web:2.51.4
+docker push zxdong262/electerm-web:2.51.4
