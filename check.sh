55cf896b85ce719e5800f922c3285d96e2517f37
ZHAO Xudong <zxdong@gmail.com>
2025-01-24 19:48:17 +0800

    Add test script

--- check.sh
+++ check.sh
@@ -3,3 +3,11 @@ docker push zxdong262/electerm-web
 docker tag zxdong262/electerm-web:latest zxdong262/electerm-web:2.60.20
 docker push zxdong262/electerm-web:2.60.20
 docker pull zxdong262/electerm-web:latest
+
+docker run --init \
+  -v "/home/execapp/dev/electerm-web/data":/home/electerm/data \
+  -v "/home/execapp/dev/electerm-web/dist":/app/electerm-web/dist \
+  -e "DB_PATH=/home/electerm/data" \
+  -e "HOST=0.0.0.0" \
+  -p 8082:5577 \
+  zxdong262/electerm-web
