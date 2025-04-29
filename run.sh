d67982edb6a679010b0290e4db18a723ab6c90a0
ZHAO Xudong <zxdong@gmail.com>
2025-04-29 18:11:50 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,12 @@
 
+#!/bin/bash
+
+echo "Starting electerm-web..."
+echo "Access URL: http://127.0.0.1:8082"
+echo "----------------------------------------"
 
 docker run --init \
-  -v "/Users/zxd/dev/dev1":/home/electerm/data \
+  -v "/Users/zxd/dev/electerm-web/data":/home/electerm/data \
   -e "DB_PATH=/home/electerm/data" \
   -e "HOST=0.0.0.0" \
   -p 8082:5577 \
