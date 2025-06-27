2149d9d8bdc08f7a0602822dde6af79fa148f8a6
ZHAO Xudong <zxdong@gmail.com>
2025-06-27 09:39:58 +0800

    Code sync

--- build.sh
+++ build.sh
@@ -17,7 +17,7 @@ if [ $? -eq 0 ]; then
   echo "Build successful. Proceeding with tagging."
 
   # Tag the latest release for all architectures
-  VERSION=2.91.17
+  VERSION=2.100.6
   docker buildx imagetools create \
     --tag zxdong262/electerm-web:$VERSION \
     zxdong262/electerm-web:latest
--- check.sh
+++ check.sh
@@ -1,7 +1,7 @@
 docker run -it zxdong262/electerm-web sh
 docker push zxdong262/electerm-web
-docker tag zxdong262/electerm-web:latest zxdong262/electerm-web:2.91.17
-docker push zxdong262/electerm-web:2.91.17
+docker tag zxdong262/electerm-web:latest zxdong262/electerm-web:2.100.6
+docker push zxdong262/electerm-web:2.100.6
 docker pull zxdong262/electerm-web:latest
 
 docker run --init \
