1d2d4b4dcde38d6f2b3f0a0b9b84f70455f543b5
ZHAO Xudong <zxdong@gmail.com>
2025-08-27 16:00:39 +0800

    Code sync

--- build/build-alpine.sh
+++ build/build-alpine.sh
@@ -22,7 +22,7 @@ if [ $? -eq 0 ]; then
   echo "Build successful. Proceeding with tagging."
 
   # Tag the latest release for all architectures
-  VERSION=2.101.10
+  VERSION=2.101.16
   docker buildx imagetools create \
     --tag zxdong262/electerm-web-alpine:$VERSION \
     zxdong262/electerm-web-alpine:latest
