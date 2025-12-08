456f96c1988892c74e606a3d70e0376ecb3f02f0
ZHAO Xudong <zxdong@gmail.com>
2025-12-08 11:10:38 +0800

    Ver -> 3.3.126

--- build.sh
+++ build.sh
@@ -17,7 +17,7 @@ if [ $? -eq 0 ]; then
   echo "Build successful. Proceeding with tagging."
 
   # Tag the latest release for all architectures
-  VERSION=3.3.118
+  VERSION=3.3.126
   docker buildx imagetools create \
     --tag zxdong262/electerm-web:$VERSION \
     zxdong262/electerm-web:latest
--- build/build-alpine.sh
+++ build/build-alpine.sh
@@ -22,7 +22,7 @@ if [ $? -eq 0 ]; then
   echo "Build successful. Proceeding with tagging."
 
   # Tag the latest release for all architectures
-  VERSION=3.3.118
+  VERSION=3.3.126
   docker buildx imagetools create \
     --tag zxdong262/electerm-web-alpine:$VERSION \
     zxdong262/electerm-web-alpine:latest
