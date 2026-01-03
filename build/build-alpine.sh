6d4be01874639f72fe7639a1248fac0cf7a4d913
ZHAO Xudong <zxdong@gmail.com>
2026-01-03 19:16:11 +0800

    Ver -> 3.3.181

--- build/build-alpine.sh
+++ build/build-alpine.sh
@@ -22,7 +22,7 @@ if [ $? -eq 0 ]; then
   echo "Build successful. Proceeding with tagging."
 
   # Tag the latest release for all architectures
-  VERSION=3.3.176
+  VERSION=3.3.181
   docker buildx imagetools create \
     --tag zxdong262/electerm-web-alpine:$VERSION \
     zxdong262/electerm-web-alpine:latest
