889612c90f199bfb01ee8187fce9f923e3c2376b
ZHAO Xudong <zxdong@gmail.com>
2026-02-18 21:22:29 +0800

    Ver -> 3.8.16

--- check.sh
+++ check.sh
@@ -1,7 +1,7 @@
 docker run -it zxdong262/electerm-web sh
 docker push zxdong262/electerm-web
-docker tag zxdong262/electerm-web:latest zxdong262/electerm-web:3.8.7
-docker push zxdong262/electerm-web:3.8.7
+docker tag zxdong262/electerm-web:latest zxdong262/electerm-web:3.8.16
+docker push zxdong262/electerm-web:3.8.16
 docker pull zxdong262/electerm-web:latest
 
 docker run --init \
