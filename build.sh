4e8a0995334aff0a8c0d69cbbabeb1a346780cc6
ZHAO Xudong <zxdong@gmail.com>
2026-09-11 17:47:13 +0800

    Ver -> 5.5.6

--- build.sh
+++ build.sh
@@ -17,7 +17,7 @@ if [ $? -eq 0 ]; then
   echo "Build successful. Proceeding with tagging."
 
   # Tag the latest release for all architectures
-  VERSION=5.3.26
+  VERSION=5.5.6
   docker buildx imagetools create \
     --tag zxdong262/electerm-web:$VERSION \
     zxdong262/electerm-web:latest
